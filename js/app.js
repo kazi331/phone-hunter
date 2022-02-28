a4c4d9de3fa2bfa0a8d1dcf6267f49672bc88d08
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2022-02-28 21:26:04 +0600

    template ready

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// focus on search input 
+const query = document.getElementById('query');
+query.focus();
+document.addEventListener('keyup', (e) => {
+    if (e.ctrlKey && e.key == '/') {
+        query.focus();
+    }
+});
+
+
+
+
+
+// enable spinner 
+function enableSpinner() {
+    countryContainer.innerHTML = `
+    <div id="spinner" class="mx-auto">
+        <div class="sk-chase">
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+    </div>
+    </div>
+    `;
+};
